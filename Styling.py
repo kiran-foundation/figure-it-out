fc9e771a9085b5ce76356a681b46e2860c53b10b
Srilatha <srilathayadalam@gmail.com>
2021-03-10 10:18:08 -0800

    Made few changes to the code

--- Styling.py
+++ Styling.py
@@ -1,61 +1,52 @@
-
 # validate function validates input for empty value and string value
 def validate():
     # Input Bust Size
-    global int_bust
-    int_bust = input("Enter Bust Size: ")
+    global bust_val
+    bust_val = input("Enter Bust Size: ")
 
     # Validating bust size
-    while int_bust == '':
-        int_bust = input("Bust size can not be empty! Enter Bust Size: ")
-
-    while not int_bust.isdigit():
-        int_bust = input("Bust size can not be string! Enter numeric value: ")
+    while bust_val == '' or bust_val.isspace():
+        bust_val = input("Bust size is invalid! Enter numeric value: ")
 
     # Input Waist Size
-    global int_waist
-    int_waist = input("Enter Waist Size: ")
+    global waist_val
+    waist_val = input("Enter Waist Size: ")
 
     # Validating waist size
-    while int_waist == '':
-        int_waist = input("Waist size can not be empty! Enter waist Size: ")
-
-    while not int_waist.isdigit():
-        int_waist = input("Waist size can not be string! Enter numeric value: ")
+    while waist_val == '' or waist_val.isspace():
+        waist_val = input("Waist size is invalid! Enter numeric value: ")
 
     # Input Hip Size
-    global int_hip
-    int_hip = input("Enter hip Size: ")
+    global hip_val
+    hip_val = input("Enter hip Size: ")
 
     # Validating hip size
-    while int_hip == '':
-        int_hip = input("Hip size can not be empty! Enter hip Size: ")
-
-    while not int_hip.isdigit():
-        int_hip = input("Hip size can not be string! Enter numeric value: ")
+    while hip_val == '' or hip_val.isspace():
+        hip_val = input("Hip size is invalid! Enter numeric value: ")
 
     # Input High Hip Size
-    global int_highhip
-    int_highhip = input("Enter High hip Size: ")
+    global highhip_val
+    highhip_val = input("Enter High hip Size: ")
 
     # Validating high hip size
-    while int_highhip == '':
-        int_highhip = input("High hip size can not be empty! Enter High hip Size: ")
-
-    while not int_highhip.isdigit():
-        int_highhip = input("High Hip size can not be string! Enter numeric value: ")
+    while highhip_val == '' or highhip_val.isspace():
+        highhip_val = input("High Hip size is invalid! Enter numeric value: ")
 
 # function to define the body type based on conditions
+
+
 def figure_it_out():
     validate()
 
     # Printing entered values
     print("\nMeasurements are:\n")
-    print(f"Bust Size is: {int_bust}")
-    print(f"Waist Size is: {int_waist}")
-    print(f"Hip Size is: {int_hip}")
-    print(f"High Hip Size is: {int_highhip}")
+    print(f"Bust Size is: {bust_val}")
+    print(f"Waist Size is: {waist_val}")
+    print(f"Hip Size is: {hip_val}")
+    print(f"High Hip Size is: {highhip_val}")
 
-    #TODO print body type based on measurements
+    # TODO print body type based on measurements
+
 
 figure_it_out()
+
