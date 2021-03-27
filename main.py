eeb4796ebf21b53c7ec517f68212dbfaab26bb66
Anuradha Chowdhary <anu2602@gmail.com>
2021-03-27 18:41:00 +0530

    updated decimal condtion

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from PIL import Image
 
 import math
 
+
 def main():
     input_val = take_input()
     convert_val = convert_str_float(input_val[0], input_val[1], input_val[2], input_val[3])
@@ -32,33 +33,34 @@ def take_input():
 
     # Validating bust size
 
-    while bust_val == '' or bust_val.isspace() or  float(bust_val) <= 9 or float(bust_val) >= 100 :
-        bust_val = input("Bust size is invalid! Enter numeric value in inch and Please enter your value in range of (9 - 99 )")
+    while bust_val == '' or bust_val.isspace() or float(bust_val) <= 9 or float(bust_val) >= 100:
+        bust_val = input(
+            "Bust size is invalid! Enter numeric value in inch and Please enter your value in range of (9 - 99 )")
 
-
-    # Input Waist Size, input_val[1]
+    # Input Waist Size #
     waist_val = input("Enter Waist Size: ")
 
     # Validating waist size
     while waist_val == '' or waist_val.isspace() or float(waist_val) <= 9 or float(waist_val) >= 100:
-        bust_val = input("Bust size is invalid! Enter numeric value in inch and Please enter your value in range of (9 - 99 )")
+        bust_val = input(
+            "Bust size is invalid! Enter numeric value in inch and Please enter your value in range of (9 - 99 )")
 
-    # Input Hip Size, input_val[2]
+    # Input Hip Size #
     hip_val = input("Enter hip Size: ")
 
     # Validating hip size
     while hip_val == '' or hip_val.isspace() or float(hip_val) <= 9 or float(hip_val) >= 100:
-        bust_val = input("Bust size is invalid! Enter numeric value in inch and Please enter your value in range of (9 - 99 )")
+        bust_val = input(
+            "Bust size is invalid! Enter numeric value in inch and Please enter your value in range of (9 - 99 )")
 
-
-    # Input High Hip Size, input_val[3]
-    global highhip_val
+    # Input High Hip Size
 
     highhip_val = input("Enter High hip Size: ")
 
     # Validating high hip size
     while highhip_val == '' or highhip_val.isspace() or float(highhip_val) <= 9 or float(highhip_val) >= 100:
-        bust_val = input("Bust size is invalid! Enter numeric value in inch and Please enter your value in range of (9 - 99 )")
+        bust_val = input(
+            "Bust size is invalid! Enter numeric value in inch and Please enter your value in range of (9 - 99 )")
 
     return bust_val, waist_val, hip_val, highhip_val
 
@@ -80,5 +82,3 @@ def repeat():
 
 
 main()
-
-
