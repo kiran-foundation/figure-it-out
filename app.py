831be945aa3631b4ab3251e2b3f7b645fb4c7c21
Srilatha <srilathayadalam@gmail.com>
2021-03-29 13:39:43 -0800

    Figure_it_out WEB App

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+
+from fio import figure_it_out
+from fio.resources import *
+from fio.figure_it_out import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    bustval = request.form.get('bust')
+    waistval = request.form.get('waist')
+    hipval = request.form.get('hip')
+    highhipval = request.form.get('highhip')
+
+    bust = float(bustval)
+    waist = float(waistval)
+    hip = float(hipval)
+    highhip = float(highhipval)
+
+    bodytype = figure_it_out.find_body_type(bust, waist, hip, highhip)
+    if type(bodytype) == tuple:
+        return render_template('submit.html', value=bodytype)
+    else:
+        return "Invalid Measurements!! Try Again. "
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
